9fc220a259a876abf965a3b7c4810a1b797d5fd8
Barbara <barbara.bergant3@gmail.com>
2020-07-06 22:41:03 +0200

    make it better

--- salon/settings.py
+++ salon/settings.py
@@ -157,7 +157,7 @@ STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
