09c658feb214843019e885dd34ec8761692eddc3
Barbara <barbara.bergant3@gmail.com>
2020-07-06 21:11:42 +0200

    make it better

--- salon/settings.py
+++ salon/settings.py
@@ -32,7 +32,7 @@ PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 's6(^9nw=9r4lu$j+bb0o!3v!u5tgb-nxp*)p*=1v=^*4^djo_x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1', 'www.kozmetika-barbara.com', 'kozmetika-barbara.com', 'salonbarbara.herokuapp.com']
 
