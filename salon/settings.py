c8a7a182751d46e71521f2760f7cdbe9599b15be
Barbara <barbara.bergant3@gmail.com>
2020-07-06 11:32:18 +0200

    sth

--- salon/settings.py
+++ salon/settings.py
@@ -141,13 +141,10 @@ MEDIA_URL = '/media/'
 
 #Email Settings
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST_USER = "barbara.bergant3@gmail.com"
+EMAIL_HOST_USER = 'barbara.bergant3@gmail.com'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = "barbara3ari"
+EMAIL_HOST_PASSWORD = 'barbara3ari'
  
-
-
 django_heroku.settings(locals())
