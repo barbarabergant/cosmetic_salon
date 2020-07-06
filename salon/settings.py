20c0dc75875a2edf868019ce1d6664ea3265e1ab
Barbara <barbara.bergant3@gmail.com>
2020-07-06 21:25:12 +0200

    make it better

--- salon/settings.py
+++ salon/settings.py
@@ -39,34 +39,17 @@ ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1', 'www.kozmetika-barbara.com', 'ko
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
-                       'pathname=%(pathname)s lineno=%(lineno)s ' +
-                       'funcname=%(funcName)s %(message)s'),
-            'datefmt': '%Y-%m-%d %H:%M:%S'
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        }
-    },
     'handlers': {
-        'null': {
-            'level': 'DEBUG',
-            'class': 'logging.NullHandler',
-        },
         'console': {
-            'level': 'DEBUG',
             'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        }
+        },
     },
     'loggers': {
-        'testlogger': {
+        'django': {
             'handlers': ['console'],
-            'level': 'INFO',
-        }
-    }
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
 }
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
