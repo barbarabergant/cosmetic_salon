a255c1c2f449519a24fe3ce2e269efd0496fd1a7
Barbara <barbara.bergant3@gmail.com>
2020-07-06 10:25:23 +0200

    b

--- salon/settings.py
+++ salon/settings.py
@@ -143,7 +143,7 @@ MEDIA_URL = '/media/'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'barbara.bergant3@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD ='barbara3ari'
 EMAIL_USE_TLS = True
 #EMAIL_USE_SSL = False
 
